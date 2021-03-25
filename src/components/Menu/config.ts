11c9b2c073a564aca2768e0d389da281ea08bd2f
Snaddyvitch-Dispenser <conor.howland@outlook.com>
2021-03-25 20:58:40 +0000

    Change colours

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -40,6 +40,10 @@ const config: MenuEntry[] = [
     icon: 'InfoIcon',
     items: [
       {
+        label: 'GitHub',
+        href: 'https://github.com/CubFinance',
+      },
+      {
         label: 'PancakeSwap',
         href: 'https://pancakeswap.info/token/0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
       },
