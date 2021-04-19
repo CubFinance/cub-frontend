53ef97a65915bdd72da23ee18b3bcfc8da9cf055
KrNel <kris.b.nelson@gmail.com>
2021-04-19 18:45:33 -0400

    chore: remove new kingdom code that is in another branch instead

--- src/views/KingdomsNew/components/Divider.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  background-color: ${({ theme }) => theme.colors.borderColor};
-  height: 1px;
-  margin: 8px auto;
-  width: 100%;
-`
--- src/views/KingdomsNew/components/DividerBlue.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  background-color: ${({ theme }) => theme.colors.textSubtle};
-  height: 1px;
-  margin: 8px auto;
-  width: 100%;
-  opacity: 0.3;
-`
