f76e50c9874decdfea9a91ee5371c47a302b0543
KrNel <kris.b.nelson@gmail.com>
2021-04-21 20:18:02 -0400

    chore: remove console log

--- src/state/farms/index.ts
+++ src/state/farms/index.ts
@@ -39,7 +39,6 @@ export const { setFarmsPublicData, setFarmUserData } = farmsSlice.actions
 // Thunks
 export const fetchFarmsPublicDataAsync = () => async (dispatch) => {
   const farms = await fetchFarms()
-  console.log('farms',farms)
   dispatch(setFarmsPublicData(farms))
 }
 export const fetchFarmUserDataAsync = (account) => async (dispatch) => {
