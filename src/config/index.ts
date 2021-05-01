c501d0daf1fe89700252d66488f2e5e44555a602
KrNel <kris.b.nelson@gmail.com>
2021-05-01 19:52:26 -0400

    fix: apr

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,8 +16,9 @@ export const BSC_BLOCK_TIME = 3
 // CAKE_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // CAKE/Block in components/CakeStats.tsx = 22 (40 - Amount sent to burn pool)
 
-export const CAKE_PER_BLOCK = new BigNumber(40)
-export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
+export const CAKE_PER_BLOCK = new BigNumber(1)
+export const BLOCKS_PER_YEAR = new BigNumber(10512000)
+// export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const BASE_URL = 'https://cubdefi.com'
 export const BASE_EXCHANGE_URL = 'https://exchange.cubdefi.com'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
