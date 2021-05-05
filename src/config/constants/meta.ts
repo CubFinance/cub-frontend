a4dab301a360037d4725b9e43496a8b737331ecb
KrNel <kris.b.nelson@gmail.com>
2021-05-05 11:35:32 -0400

    fix: tab title

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,44 +1,44 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
+  title: 'Cub Finance',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by Cub Finance), NFTs, and more, on a platform you can trust.',
+  image: 'https://cubdefi.com/images/2logos',
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
   '/': {
-    title: 'Home | PancakeSwap',
+    title: 'Home | Cub Finance',
   },
   '/competition': {
-    title: 'Trading Battle | PancakeSwap',
+    title: 'Trading Battle | Cub Finance',
   },
   '/prediction': {
-    title: 'Prediction | PancakeSwap',
+    title: 'Prediction | Cub Finance',
   },
   '/farms': {
-    title: 'Farms | PancakeSwap',
+    title: 'Farms | Cub Finance',
   },
   '/pools': {
-    title: 'Pools | PancakeSwap',
+    title: 'Pools | Cub Finance',
   },
   '/lottery': {
-    title: 'Lottery | PancakeSwap',
+    title: 'Lottery | Cub Finance',
   },
   '/collectibles': {
-    title: 'Collectibles | PancakeSwap',
+    title: 'Collectibles | Cub Finance',
   },
   '/ifo': {
-    title: 'Initial Farm Offering | PancakeSwap',
+    title: 'Initial Farm Offering | Cub Finance',
   },
   '/teams': {
-    title: 'Leaderboard | PancakeSwap',
+    title: 'Leaderboard | Cub Finance',
   },
   '/profile/tasks': {
-    title: 'Task Center | PancakeSwap',
+    title: 'Task Center | Cub Finance',
   },
   '/profile': {
-    title: 'Your Profile | PancakeSwap',
+    title: 'Your Profile | Cub Finance',
   },
 }
