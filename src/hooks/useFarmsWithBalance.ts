6ebfdbc5126bb9cf570a60dac822b1e29fe66362
KrNel <kris.b.nelson@gmail.com>
2021-05-29 01:15:44 -0400

    fix: kingdoms not showing up in home harvest all

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react'
 import BigNumber from 'bignumber.js'
 import { useWeb3React } from '@web3-react/core'
 import multicall from 'utils/multicall'
-import { getMasterChefAddress } from 'utils/addressHelpers'
+import { getMasterChefAddress, getKingdomsAddress } from 'utils/addressHelpers'
 import masterChefABI from 'config/abi/masterchef.json'
+import kingdomsABI from 'config/abi/kingdoms.json'
 import { farmsConfig } from 'config/constants'
 import { FarmConfig } from 'config/constants/types'
 import useRefresh from './useRefresh'
@@ -19,16 +20,39 @@ const useFarmsWithBalance = () => {
 
   useEffect(() => {
     const fetchBalances = async () => {
-      const calls = farmsConfig.map((farm) => ({
-        address: getMasterChefAddress(),
-        name: 'pendingCub',
-        params: [farm.pid, account],
-      }))
+      let calls = farmsConfig.map((farm) => {
+        if (!farm.isKingdom) {
+          return {
+            address: getMasterChefAddress(),
+            name: 'pendingCub',
+            params: [farm.pid, account],
+          }
+        }
+        return null
+      })
+
+      calls = calls.filter(call => call)
 
       const rawResults = await multicall(masterChefABI, calls)
       const results = farmsConfig.map((farm, index) => ({ ...farm, balance: new BigNumber(rawResults[index]) }))
 
-      setFarmsWithBalances(results)
+      let callsK = farmsConfig.map((farm) => {
+        if (farm.isKingdom) {
+          return {
+            address: getKingdomsAddress(),
+            name: 'pendingCUB',
+            params: [farm.pid, account],
+          }
+        }
+        return null
+      })
+
+      callsK = callsK.filter(call => call)
+
+      const rawResultsK = await multicall(kingdomsABI, callsK)
+      const resultsK = farmsConfig.map((farm, index) => ({ ...farm, balance: new BigNumber(rawResultsK[index]) }))
+
+      setFarmsWithBalances([...results, ...resultsK])
     }
 
     if (account) {
