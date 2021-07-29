23577de91ba62b7ca7dfe2225d27f1d9df452d40
KrNel <kris.b.nelson@gmail.com>
2021-07-28 23:20:04 -0400

    chore: build

--- build/static/js/10.53ed4d79.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcubdefi-frontend"]=this["webpackJsonpcubdefi-frontend"]||[]).push([[10],{909:function(n,e){}}]);
-//# sourceMappingURL=10.53ed4d79.chunk.js.map
---
+++ build/static/js/10.87685ede.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcubdefi-frontend"]=this["webpackJsonpcubdefi-frontend"]||[]).push([[10],{911:function(n,e){}}]);
+//# sourceMappingURL=10.87685ede.chunk.js.map
